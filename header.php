3f11b15f93f041169808631fabce69c62de2d25f
Ash <ashley.gordon2@gmail.com>
2022-05-25 11:11:16 +0100

    add GA code and add css font-display

--- header.php
+++ header.php
@@ -18,6 +18,15 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-M7KRNQWS3J"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-M7KRNQWS3J');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
